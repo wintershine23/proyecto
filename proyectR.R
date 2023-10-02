63d296c8536fd36754cc9c4fe37c1141a820ec2d
Ester Aguilar <you@example.com>
2023-10-02 22:40:18 +0200

    Comentarios de Ester

--- proyectR.R
+++ proyectR.R
@@ -4,3 +4,7 @@
 
 
 # Esto funciona? (Esto lo ha puesto Ester)
+# Si te metes en tu cuenta, te aparece como que YO tengo un repositorio
+# que se llama LiChuan1234/proyecto? Porque a mí me sale que tú tienes
+# un repositorio que se llama "tu usuario/el nombre del respositorio del trabajo"
+# qué está pasando lol
